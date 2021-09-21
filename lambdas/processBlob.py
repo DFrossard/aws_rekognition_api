cfba72253f1ef09653f77c622078092c6f4d5b44
Daniel Frossard <frossard.daniel@gmail.com>
2021-09-20 23:47:41 -0300

    added error handling on processBlob

--- lambdas/processBlob.py
+++ lambdas/processBlob.py
@@ -1,28 +1,53 @@
 import os
 import json
+import logging
 import boto3
+import botocore
 from commom.dynamodb import DynamoDB
+from commom.responses import Responses
 
 def handler(event, context):
+    responses = Responses()
     bucket = os.environ['bucketName']
     image_key = event['Records'][0]['s3']['object']['key']
 
-    labels = get_labels(bucket, image_key)
+    try:
+        labels = get_labels(bucket, image_key)
+    except:
+        msg = f"Error while detecting image labels on image {image_key}"
+        logging.error(msg)
+        return responses._500_response(msg)
+
     formatted_labels = format_labels(labels)
 
     id = image_key.split('/')[0]
-    blob = retrieve_item(id)
+    try:
+        blob = retrieve_item(id)
+    except:
+        msg = f"Error while retrieving item from database for image {image_key}"
+        logging.error(msg)
+        return responses._500_response(msg)
+
+    
     blob_item = blob['Item']
-    save_response = save_labels(formatted_labels, blob_item)
+    try:
+        save_response = save_labels(formatted_labels, blob_item)
+    except:
+        msg = f"Error while detecting image labels on image {image_key}"
+        logging.error(msg)
+        return responses._500_response(msg)
     status_code = save_response['ResponseMetadata']['HTTPStatusCode']
-    response = {"statusCode": status_code, "body": json.dumps(save_response)}
+    response = responses._200_response(f"Image {image_key} processed")
 
 
     return response
 
 def get_labels(bucket, image_key):
     client = boto3.client("rekognition")
-    labels_response = client.detect_labels(Image = {"S3Object": {"Bucket": bucket, "Name": image_key}}, MaxLabels=5,  MinConfidence=80)
+    try:
+        labels_response = client.detect_labels(Image = {"S3Object": {"Bucket": bucket, "Name": image_key}}, MaxLabels=5,  MinConfidence=80)
+    except botocore.exceptions.ClientError as error:
+        raise error
     return labels_response
 
 def format_labels(labels):
